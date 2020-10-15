972c0357fea766481f50070026d8a45b61391de1
jason <jasonl8121@foxmail.com>
2020-10-15 20:44:58 +0800

    GBNINIT

---
+++ GBN/GBN.cpp
@@ -0,0 +1,32 @@
+// StopWait.cpp : 定义控制台应用程序的入口点。
+//
+
+
+#include "Global.h"
+#include "RdtSender.h"
+#include "RdtReceiver.h"
+#include "GBNRdtSender.h"
+#include "GBNRdtReceiver.h"
+
+
+int main(int argc, char* argv[])
+{
+	RdtSender *ps = new GBNRdtSender();
+	RdtReceiver * pr = new GBNRdtReceiver();
+	// pns->setRunMode(0);  //VERBOS模式
+	pns->setRunMode(1);  //安静模式
+	pns->init();
+	pns->setRtdSender(ps);
+	pns->setRtdReceiver(pr);
+	pns->setInputFile("D:\\StopWait\\input.txt");
+	pns->setOutputFile("D:\\StopWait\\output.txt");
+
+	pns->start();
+
+	delete ps;
+	delete pr;
+	delete pUtils;									//指向唯一的工具类实例，只在main函数结束前delete
+	delete pns;										//指向唯一的模拟网络环境类实例，只在main函数结束前delete
+	
+	return 0;
+}
